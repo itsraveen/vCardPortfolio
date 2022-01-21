43ce1779ca81eed8c6ee491183354e4228a098e9
Raveen Prabhu <raveenprabhu99@gmail.com>
2022-01-21 22:50:08 +0800

    First Commit

---
+++ index.js
@@ -0,0 +1,53 @@
+const buttons = document.querySelectorAll(".card-buttons button");
+const sections = document.querySelectorAll(".card-section");
+const card = document.querySelector(".card");
+
+const handleButtonClick = e => {
+  const targetSection = e.target.getAttribute("data-section");
+  const section = document.querySelector(targetSection);
+  targetSection !== "#about" ?
+  card.classList.add("is-active") :
+  card.classList.remove("is-active");
+  card.setAttribute("data-state", targetSection);
+  sections.forEach(s => s.classList.remove("is-active"));
+  buttons.forEach(b => b.classList.remove("is-active"));
+  e.target.classList.add("is-active");
+  section.classList.add("is-active");
+};
+
+buttons.forEach(btn => {
+  btn.addEventListener("click", handleButtonClick);
+});
+
+var worktgtbtn = document.querySelector(".contact-me").addEventListener("click", handleContactBtn)
+var learnmorebtn = document.querySelector(".linked-in").addEventListener("click", handleLearnMoreBtn)
+var freshmankitbtn = document.querySelector(".freshman-kit").addEventListener("click", handleFreshmanKitBtn)
+var emcccrewbtn = document.querySelector(".emcc-crew").addEventListener("click", handleEMCCCrewBtn)
+var dscnusbtn = document.querySelector(".dsc-nus").addEventListener("click", handleDSCNUSBtn)
+
+
+var dscnusbtn = document.querySelector(".linkedin").addEventListener("click", handleLinkedInBtn)
+
+function handleLinkedInBtn(){
+    window.location.href = "http://www.linkedin.com/in/raveenprabhu"
+}
+
+function handleContactBtn(){
+    window.location.href = "https://jeremycte.com/#contact";
+};
+
+function handleLearnMoreBtn(){
+  window.location.href = "https://jeremycte.com/#about";
+}
+
+function handleFreshmanKitBtn(){
+  window.location.href = "https://nuscomputingfreshmankit.surge.sh";
+}
+
+function handleEMCCCrewBtn() {
+  window.location.href = "https://nusemcccrew.com/training"
+}
+
+function handleDSCNUSBtn() {
+  window.location.href = "https://www.youtube.com/c/JoelWongJY"
+}
